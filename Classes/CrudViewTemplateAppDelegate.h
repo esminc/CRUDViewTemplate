3ae8e84df18ca37ee9eafc0abe9a2f1b2524b495
h-morita <h-morita@esm.co.jp>
2009-04-28 09:41:14 +0900

    create project.

---
+++ Classes/CrudViewTemplateAppDelegate.h
@@ -0,0 +1,29 @@
+//
+//  CrudViewTemplateAppDelegate.h
+//  CrudViewTemplate
+//
+//  Created by MORITA Hideyuki on 09/04/28.
+//  Copyright Eiwa System Management, Inc 2009. All rights reserved.
+//
+
+@interface CrudViewTemplateAppDelegate : NSObject <UIApplicationDelegate> {
+
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;	    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+    UIWindow *window;
+}
+
+- (IBAction)saveAction:sender;
+
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+@property (nonatomic, readonly) NSString *applicationDocumentsDirectory;
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CrudViewTemplate
+//
+//  Created by MORITA Hideyuki on 09/04/28.
+//  Copyright Eiwa System Management, Inc 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
